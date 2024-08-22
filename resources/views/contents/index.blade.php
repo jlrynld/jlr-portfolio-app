cd59b3daf49891038dda508a0a933ad9a40b00c1
Jlrynld <jlbrynld@gmail.com>
2024-08-22 21:44:54 +0800

    updated responsiveness

--- resources/views/contents/index.blade.php
+++ resources/views/contents/index.blade.php
@@ -66,55 +66,55 @@
 
     <br>
 
-        <div class="col-md-12 mx-auto mt-5">
-            <div class="card mt-3 animated-card portfolio-card shadow rounded">
-                <div class="card-body text-color">
-                    <h3 class="card-title">/ skills & tools</h3>
+    <div class="col-md-12 mx-auto mt-5">
+        <div class="card mt-3 animated-card portfolio-card shadow rounded">
+            <div class="card-body text-color">
+                <h3 class="card-title">/ skills & tools</h3>
 
-                        <div class="mt-2 mx-2">
-                            <h5 class="card-text mt-4">Web Development</h5>
-                        </div>
-
-                        <div class="d-flex justify-content-start mt-4">
-                            <i class='bx bxs-file-html text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                            <i class='bx bxs-file-css text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                            <i class='bx bxl-php text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                            <i class='bx bxl-javascript text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                        </div>
-
-                        <div class="d-flex justify-content-start mt-2">
-                            <i class='bx bxl-bootstrap text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                            <i class='bx bxl-visual-studio text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                            <i class='bx bxl-wordpress text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                            <i class='bx bxl-github text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                        </div>
-
-                        <div class="d-flex justify-content-start mt-2">
-                            <img src="/css/images/Laravel-icon.png" class="text-color border rounded mx-2 text-center" style="width: 20%; height: 100px; object-fit: contain;">
-                            <img src="/css/images/SQL-icon.png" class="text-color border rounded mx-2 text-center" style="width: 20%; height: 100px; object-fit: contain;">
-                            <i class='bx bxl-java text-color border rounded mx-2 text-center' style="font-size: 100px; width: 20%;"></i>
-                        </div>
-
-                        <h6 class="card-text mt-4 mx-2 fw-light">
-                            I have a strong background in web development, particularly using Laravel to build dynamic websites. <br> I mainly use Visual Studio Code as my Text Editor or IDE.
-                        </h6>
-
-                        <h6 class="card-text mt-4 mx-2 fw-light">
-                            For version control and project collaboration, I rely on GitHub, where I manage all my repositories. <br>
-                            Additionally, I also have experience in using Hostinger for web hosting and domain management, ensuring a smooth deployment process.
-                        </h6>
-
-                        <div class="mx-2 mt-5">
-                            <h5 class="card-text mt-4">Tech Support</h5>
-                            <h6 class="card-text fw-light">
-                                On the tech support side, I’m capable of troubleshooting, OS installations, and building PCs. <br>
-                                I can also crimp LAN cables, design LAN setups for multiple PCs connected to a switch, configure routers for both static IPs and DHCP, and set up PCs efficiently.
-                            </h6>
-                        </div>
+                <div class="mt-2 mx-2">
+                    <h5 class="card-text mt-4">Web Development</h5>
                 </div>
-                        <section id="projects"></section>
+
+                <div class="d-flex justify-content-start mt-4">
+                    <i class='bx bxs-file-html text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                    <i class='bx bxs-file-css text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                    <i class='bx bxl-php text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                    <i class='bx bxl-javascript text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                </div>
+
+                <div class="d-flex justify-content-start mt-2">
+                    <i class='bx bxl-bootstrap text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                    <i class='bx bxl-visual-studio text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                    <i class='bx bxl-wordpress text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                    <i class='bx bxl-github text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                </div>
+
+                <div class="d-flex justify-content-start mt-2">
+                    <img src="/css/images/Laravel-icon.png" class="text-color border rounded mx-2 text-center img-icon" style="height: 100px; object-fit: contain;">
+                    <img src="/css/images/SQL-icon.png" class="text-color border rounded mx-2 text-center img-icon" style="height: 100px; object-fit: contain;">
+                    <i class='bx bxl-java text-color border rounded mx-2 text-center icon' style="font-size: 100px;"></i>
+                </div>
+
+                <h6 class="card-text mt-4 mx-2 fw-light">
+                    I have a strong background in web development, particularly using Laravel to build dynamic websites. <br> I mainly use Visual Studio Code as my Text Editor or IDE.
+                </h6>
+
+                <h6 class="card-text mt-4 mx-2 fw-light">
+                    For version control and project collaboration, I rely on GitHub, where I manage all my repositories. <br>
+                    Additionally, I also have experience in using Hostinger for web hosting and domain management, ensuring a smooth deployment process.
+                </h6>
+
+                <div class="mx-2 mt-5">
+                    <h5 class="card-text mt-4">Tech Support</h5>
+                    <h6 class="card-text fw-light">
+                        On the tech support side, I’m capable of troubleshooting, OS installations, and building PCs. <br>
+                        I can also crimp LAN cables, design LAN setups for multiple PCs connected to a switch, configure routers for both static IPs and DHCP, and set up PCs efficiently.
+                    </h6>
+                </div>
             </div>
+            <section id="projects"></section>
         </div>
+    </div>
 
         <br>
 
