facde4b8d7d9faac927ebb41e9ca7eb9ebde2bf8
Jlrynld <jlbrynld@gmail.com>
2024-12-10 02:04:23 +0800

    updated Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,9 +9,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
