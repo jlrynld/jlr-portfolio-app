71d0a7b75cdee1282bef6403c30ef22bd9758e25
Jlrynld <jlbrynld@gmail.com>
2024-12-13 18:47:35 +0800

    updated about

--- resources/views/contents/index.blade.php
+++ resources/views/contents/index.blade.php
@@ -30,7 +30,7 @@
                 <h3 class="card-title">/ about</h3>
                 <p class="card-text mt-3 fw-light" style="font-size: 18px;">I'm an Aspiring Web Developer based in the Philippines.
                     Currently focused on enhancing my skills in Web Development.
-                    I graduated with a Bachelor of Science in Information Technology from AMA Computer College on September 10, 2024.</p>
+                    I graduated with a Bachelor of Science in Information Technology from AMA Computer College on September of 2024.</p>
             </div>
             <section id="history"></section>
         </div>
