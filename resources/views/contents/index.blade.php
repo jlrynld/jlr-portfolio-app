db4c2457214ff8fee6835283821e44a67db4eea3
Jlrynld <jlbrynld@gmail.com>
2024-12-17 04:47:45 +0800

    `Removed 1x1.JPG from public/css/images and updated image source in index.blade.php and app.blade.php to 1x1.png`

--- resources/views/contents/index.blade.php
+++ resources/views/contents/index.blade.php
@@ -7,7 +7,7 @@
     <div data-aos="zoom-in" class="col-md-8 mx-auto" style="padding-bottom: 410px;">
         <div class="card mt-3 portfolio-card shadow rounded">
             <div class="card-body text-color d-flex flex-column flex-md-row align-items-center">
-                <img src="/css/images/1x1.jpg" class="profile-img mb-3" style="height: 250px; width: 250px; border-radius: 50%; object-fit: cover; margin-right: 20px;">
+                <img src="/css/images/1x1.png" class="profile-img mb-3" style="height: 250px; width: 250px; border-radius: 50%; object-fit: cover; margin-right: 20px;">
                 <div>
                     <h5 class="card-title fw-light">Hi, my name is</h5>
                     <h3 class="card-text">John Louie B. Reynaldo</h3>
